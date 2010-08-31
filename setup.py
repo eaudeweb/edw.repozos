328c29331d9d42e9e09a3e60b6ccd9b783df19ef
Alex Morega <alex@grep.ro>
2010-08-31 18:28:26 +0300

    update setup.py after merge

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='edw.repozos',
-    version='0.1',
+    version='0.2',
     author='Eau de Web',
     author_email='office@eaudeweb.ro',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    entry_points={'console_scripts': ['do_repozos = edw.repozos.backup:main']},
+    entry_points={'console_scripts': [
+        'do_backup = edw.repozos.backup:main',
+        'do_cleanup = edw.repozos.cleanup:main',
+    ]},
 )
